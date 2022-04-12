85f29a264c781f68d39d4cc7084979060df681c3
Jingxun <Jingxun0518@163.com>
2022-04-12 23:20:44 +0800

    first commit dir

--- git_idea/src/main/java/com/atguigu/demo1/Git.java
+++ git_idea/src/main/java/com/atguigu/demo1/Git.java
@@ -3,6 +3,7 @@ package com.atguigu.demo1;
 public class Git {
     public static void main(String... args){
         System.out.println("1");
+        System.out.println("1");
 
     }
 }
---
+++ git_idea/src/main/java/com/atguigu/demo1/GitTest.java
@@ -0,0 +1,9 @@
+package com.atguigu.demo1;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("2");
+    }
+
+}
+
